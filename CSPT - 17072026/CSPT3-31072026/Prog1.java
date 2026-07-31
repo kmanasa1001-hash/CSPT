29f8d994e221efaa6bf1ba685af22ca4e732550b
kmanasa1001-hash <k.manasa1001@gmail.com>
2026-07-31 13:38:57 +0000

    day3

---
+++ CSPT - 17072026/CSPT3-31072026/Prog1.java
@@ -0,0 +1,17 @@
+class Demostration{
+    public static void main(string[] args){
+        Demostration d = new Demostration();
+        Demostration d1 = new Demostration();
+        IO.println(d.getClass());
+        IO.println(System.out.getClass());
+        Integer i1 = new integer(10);
+        Integer i1 = new integer (10);
+        IO.println(d.equals(d1) + "---" + i1.equals(i2));
+        IO.print(d.hashCode() + "---"+ d1.hashCode());
+        IO.print(d.hashCode() + "---"+ i1.hashCode());
+        
+
+
+    }
+
+}
